2cffd8c53a7e40771fea62ce1178bbe42eccd3d9
Ali <52371550+komesogutluali@users.noreply.github.com>
2022-02-14 01:59:15 +0300

    Update AccountService.java

--- simplebanking-master/src/main/java/com/eteration/simplebanking/services/AccountService.java
+++ simplebanking-master/src/main/java/com/eteration/simplebanking/services/AccountService.java
@@ -3,6 +3,7 @@ package com.eteration.simplebanking.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.eteration.simplebanking.config.AuthenticationConfigConstants;
+import com.eteration.simplebanking.config.PersistenceConfig;
 import com.eteration.simplebanking.entities.BankAccount;
 import com.eteration.simplebanking.entities.Transaction;
 
@@ -14,7 +15,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import javax.persistence.EntityTransaction;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.Base64;
@@ -24,52 +25,35 @@ import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
-
+    EntityTransaction et;
     @Autowired
     private BankAccountRepository bankAccountRepository;
     @Autowired
     TransactionRepository transactionRepository;
     @Autowired
     TransactionTypeRepository transactionTypeRepository;
+    @Autowired
+    PersistenceConfig persistenceConfig;
+
     public BankAccount findAccount(String accountNumber) {
         BankAccount bankAccount=bankAccountRepository.findAll().stream().filter(b->b.getAccountnumber().equals(accountNumber)).findFirst().get();
     return bankAccount;
     }
     public String Credit(String accountNumber, Amount amount)
     {
-        String localDateTime=LocalDateTime.now().toString();
-        BankAccount bankAccount=bankAccountRepository.findAll().stream().filter(b->b.getAccountnumber().equals(accountNumber)).findFirst().get();
-        Transaction transaction=new Transaction();
-        transaction.setAmount(amount.getAmount().doubleValue());
-        transaction.setTransactionType(transactionTypeRepository.findAll().stream().filter(t->t.getId().equals(1)).findFirst().get());
-        transaction.setBankAccount(bankAccount);
-        transaction.setCreateDate(LocalDateTime.now().toString());
-        bankAccount.setBalance(bankAccount.getBalance()+amount.getAmount().doubleValue());
-        String token = JWT.create()
-                .withSubject(Base64.getEncoder().encodeToString((localDateTime+"."+amount+"."+transaction.getTransactionType().getType()+"."+bankAccount.getId()).getBytes(StandardCharsets.UTF_8)))
-                .withExpiresAt(new Date(System.currentTimeMillis()))
-                .sign(Algorithm.HMAC256(AuthenticationConfigConstants.SECRET.getBytes()));
-        transaction.setApprovalcode(token);
-        bankAccountRepository.save(bankAccount);
-        transactionRepository.save(transaction);
         JSONObject response=new JSONObject();
-        response.put("status","OK");
-        response.put("approvalCode",token);
-
-        return response.toString();
-    }
-    public String Debit(String accountNumber,Amount amount)
-    {
-        BankAccount bankAccount=bankAccountRepository.findAll().stream().filter(b->b.getAccountnumber().equals(accountNumber)).findFirst().get();
-        if(bankAccount.getBalance().doubleValue()!=0)
+        et = persistenceConfig.entityManagerFactory().getNativeEntityManagerFactory().createEntityManager().getTransaction();
+        try
         {
+            et.begin();
             String localDateTime=LocalDateTime.now().toString();
+            BankAccount bankAccount=bankAccountRepository.findAll().stream().filter(b->b.getAccountnumber().equals(accountNumber)).findFirst().get();
             Transaction transaction=new Transaction();
             transaction.setAmount(amount.getAmount().doubleValue());
-            transaction.setTransactionType(transactionTypeRepository.findAll().stream().filter(t->t.getId().equals(2)).findFirst().get());
+            transaction.setTransactionType(transactionTypeRepository.findAll().stream().filter(t->t.getId().equals(1)).findFirst().get());
             transaction.setBankAccount(bankAccount);
-            transaction.setCreateDate(localDateTime);
-            bankAccount.setBalance(bankAccount.getBalance()-amount.getAmount().doubleValue());
+            transaction.setCreateDate(LocalDateTime.now().toString());
+            bankAccount.setBalance(bankAccount.getBalance()+amount.getAmount().doubleValue());
             String token = JWT.create()
                     .withSubject(Base64.getEncoder().encodeToString((localDateTime+"."+amount+"."+transaction.getTransactionType().getType()+"."+bankAccount.getId()).getBytes(StandardCharsets.UTF_8)))
                     .withExpiresAt(new Date(System.currentTimeMillis()))
@@ -77,13 +61,53 @@ public class AccountService {
             transaction.setApprovalcode(token);
             bankAccountRepository.save(bankAccount);
             transactionRepository.save(transaction);
-            JSONObject response=new JSONObject();
+            et.commit();
             response.put("status","OK");
             response.put("approvalCode",token);
-            return response.toString();
+        }catch (Exception exception)
+        {
+            et.rollback();
+            response.put("status","Error");
         }
-        return "0";
+
+        return response.toString();
     }
+    public String Debit(String accountNumber,Amount amount)
+    {
+        JSONObject response=new JSONObject();
+        et = persistenceConfig.entityManagerFactory().getNativeEntityManagerFactory().createEntityManager().getTransaction();
+        try {
+            et.begin();
+            BankAccount bankAccount = bankAccountRepository.findAll().stream().filter(b -> b.getAccountnumber().equals(accountNumber)).findFirst().get();
+            if (bankAccount.getBalance().doubleValue() != 0) {
+                String localDateTime = LocalDateTime.now().toString();
+                Transaction transaction = new Transaction();
+                transaction.setAmount(amount.getAmount().doubleValue());
+                transaction.setTransactionType(transactionTypeRepository.findAll().stream().filter(t -> t.getId().equals(2)).findFirst().get());
+                transaction.setBankAccount(bankAccount);
+                transaction.setCreateDate(localDateTime);
+                bankAccount.setBalance(bankAccount.getBalance() - amount.getAmount().doubleValue());
+                String token = JWT.create()
+                        .withSubject(Base64.getEncoder().encodeToString((localDateTime + "." + amount + "." + transaction.getTransactionType().getType() + "." + bankAccount.getId()).getBytes(StandardCharsets.UTF_8)))
+                        .withExpiresAt(new Date(System.currentTimeMillis()))
+                        .sign(Algorithm.HMAC256(AuthenticationConfigConstants.SECRET.getBytes()));
+                transaction.setApprovalcode(token);
+                bankAccountRepository.save(bankAccount);
+                transactionRepository.save(transaction);
+                response.put("status", "OK");
+                response.put("approvalCode", token);
+                et.commit();
+            }
+        }
+        catch (Exception ex)
+        {
+            et.rollback();
+            response.put("status","Error");
+        }
+        return response.toString();
+        }
+
+
     public String AccountCurrentData(String accountNumber)
     {
         BankAccount bankAccount=bankAccountRepository.findAll().stream().filter(b->b.getAccountnumber().equals(accountNumber)).findFirst().get();
